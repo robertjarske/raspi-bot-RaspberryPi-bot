a460351c8790031c8ee6aa00a5edf410e790da17
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-03 10:28:06 +0200

    add robot model

---
+++ backend/models/Robot.js
@@ -0,0 +1,14 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_update"] }] */
+const mongoose = require('mongoose');
+
+
+const robotSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  ip: { type: String, required: true },
+  isOnline: { type: Boolean, default: false },
+  isAvailable: { type: Boolean, default: false },
+});
+
+const Robot = mongoose.model('Robot', robotSchema);
+
+module.exports = Robot;
