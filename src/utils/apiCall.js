84801f5e7ff1d8e7a11d2becb567b6bfc2a289e3
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-10 12:58:10 +0200

    refactor apicall to be curried

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -1,11 +1,29 @@
-export const apiCall = async (url) => {
-  const response = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+import _ from 'lodash';
 
+// export const apiCall = async (url) => {
+//   const response = await fetch(url, {
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+//   const data = await response.json();
+
+//   return data;
+// };
+
+const apiCall = async (url, subUrl, request) => {
+  const fetchUrl = subUrl ? `${url}/${subUrl}` : url;
+
+
+  const response = await fetch(fetchUrl, request);
   const data = await response.json();
 
-  return data;
+  if (!response.ok) {
+    return Promise.reject(data);
+  }
+
+  return Promise.resolve(data);
 };
+
+export const curriedApiCall = _.curry(apiCall);
