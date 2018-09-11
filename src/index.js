2b464022a777b9db91976057d9fbb4a4e463a5af
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-11 15:53:18 +0200

    add global css and variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 require('dotenv').config();
 
