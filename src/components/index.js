fbeda9fea8c1345a0a086e41e7c52a758460626c
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-14 15:55:42 +0200

    add logo to exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Form } from './Form/Form';
 export { default as PrivateRoute } from './PrivateRoute/PrivateRoute';
 export { default as withPublicRoot } from './withPublicRoot/withPublicRoot';
 export { default as Stream } from './Stream/Stream';
+export { default as Logo } from './Logo/Logo';
