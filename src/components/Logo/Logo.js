c6afbc8114e21d287ebbf1c51d7651022af18f0f
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-14 15:55:14 +0200

    update logo layout

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './Logo.css';
 
-const Logo = ({ ...props }) => (
+const Logo = () => (
   <div className="logo_container">
     <img src={logo} className="logo" alt="logo" />
     <div className="logo_text">
-      <h1 className="App-title">NodeBot Driver</h1>
-      <p>
-        Also connected to Pi:
-        {props.backend}
-      </p>
+      <Link to={'/'} className="App-title">NodeBot <span className="logo-span">Driver</span></Link>
     </div>
   </div>
 );
