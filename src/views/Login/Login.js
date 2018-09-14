3872e3163c6eb4a26828220b15a953e899dc8454
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-14 15:56:26 +0200

    add redirects if logged in

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Form } from '../../components';
 import { requestLogin } from '../../redux/auth/actions';
@@ -13,14 +14,19 @@ const mapDispatchToProps = dispatch => ({
   requestLogin: credentials => dispatch(requestLogin(credentials)),
 });
 
-const Login = ({ ...props }) => (
-  <div className="content">
+const Login = ({ ...props }) => {
+  if (props.isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
-    <Form loginRequest={props.requestLogin} kindof={'login'}/>
-    <div className="background"></div>
-  </div>
-);
+  return (
+    <div className="content">
 
+      <Form loginRequest={props.requestLogin} kindof={'login'}/>
+      <div className="background"></div>
+    </div>
+  );
+};
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
--- src/views/Signup/Signup.js
+++ src/views/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { Form } from '../../components';
 import { requestSignup } from '../../redux/auth/actions';
 import './Signup.css';
@@ -13,12 +14,18 @@ const mapDispatchToProps = dispatch => ({
   requestSignup: credentials => dispatch(requestSignup(credentials)),
 });
 
-const Signup = ({ ...props }) => (
-  <div className="content">
-    <Form signupRequest={props.requestSignup} kindof={'signup'}/>
-    <div className="background"></div>
-  </div>
-);
+const Signup = ({ ...props }) => {
+  if (props.isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div className="content">
+      <Form signupRequest={props.requestSignup} kindof={'signup'}/>
+      <div className="background"></div>
+    </div>
+  );
+};
 
 export default connect(
   mapStateToProps,
