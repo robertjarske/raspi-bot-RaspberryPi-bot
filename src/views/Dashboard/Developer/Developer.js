8cd701c72fa2ee6746996a082c134a8fa17b4d97
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-15 10:12:20 +0200

    linted documents

--- src/views/Dashboard/Developer/Developer.js
+++ src/views/Dashboard/Developer/Developer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Developer = ({ ...props }) => {
   console.log(props);
   return (
-    <div style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black' }}>This is Developer</div>
+    <div style={{
+      width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black',
+    }}>This is Developer</div>
   );
 };
 export default Developer;
