fcf28023a2965ea8e70d367af0101e8739db2578
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-16 12:58:45 +0200

    update admin to regular component, add admin.css

--- src/views/Dashboard/Admin/Admin.js
+++ src/views/Dashboard/Admin/Admin.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './Admin.css';
 
-const Admin = ({ ...props }) => {
-  console.log(props);
-  return (
-    <div style={{
-      width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black',
-    }}>This is Admin</div>
-  );
-};
+class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log('get all users');
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div style={{
+        width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black',
+      }}>This is Admin</div>
+    );
+  }
+}
+
 export default Admin;
