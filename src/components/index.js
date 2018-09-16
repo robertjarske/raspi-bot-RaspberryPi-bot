9347ce32aa4693bfb966a4be2b8557f32d2f7be2
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-16 14:02:04 +0200

    add loader to default exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as PrivateRoute } from './PrivateRoute/PrivateRoute';
 export { default as withPublicRoot } from './withPublicRoot/withPublicRoot';
 export { default as Stream } from './Stream/Stream';
 export { default as Logo } from './Logo/Logo';
+export { default as Loader } from './Loader/Loader';
