e726577c6a9ff7afa40dea623409a2dfc3772a0a
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-16 13:51:20 +0200

    add request all users for admin

--- src/redux/user/actions.js
+++ src/redux/user/actions.js
@@ -2,18 +2,51 @@ import {
   REQUEST_UPDATE_USER_START,
   REQUEST_UPDATE_USER_SUCCESS,
   REQUEST_UPDATE_USER_FAIL,
+  REQUEST_USERS_START,
+  REQUEST_USERS_SUCCESS,
+  REQUEST_USERS_FAIL,
 } from './constants';
 
 import { curriedApiCall } from '../../utils/apiCall';
 
 const userApiCall = curriedApiCall(`${process.env.REACT_APP_API_URL}/users`);
 
+/** Get all users(admin) */
+export const requestUsersStart = () => ({ type: REQUEST_USERS_START });
 
+export const requestUsersSuccess = data => ({
+  type: REQUEST_USERS_SUCCESS,
+  payload: data,
+});
+
+export const requestUsersFail = err => ({
+  type: REQUEST_USERS_FAIL,
+  payload: err,
+});
+
+export const requestUsers = () => (dispatch) => {
+  dispatch(requestUsersStart());
+
+  userApiCall('/', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': localStorage.getItem('token'),
+    },
+  })
+    .then(data => dispatch(requestUsersSuccess(data)))
+    .catch(err => dispatch(requestUsersFail(err)));
+};
+
+
+/** Update User(self) */
 export const requestUserUpdateStart = () => ({ type: REQUEST_UPDATE_USER_START });
+
 export const requestUserUpdateSuccess = data => ({
   type: REQUEST_UPDATE_USER_SUCCESS,
   payload: data,
 });
+
 export const requestUserUpdateFail = err => ({
   type: REQUEST_UPDATE_USER_FAIL,
   payload: err,
--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -9,10 +9,14 @@ import {
   REQUEST_UPDATE_USER_START,
   REQUEST_UPDATE_USER_SUCCESS,
   REQUEST_UPDATE_USER_FAIL,
+  REQUEST_USERS_START,
+  REQUEST_USERS_SUCCESS,
+  REQUEST_USERS_FAIL,
 } from './constants';
 
 const initialState = {
   user: {},
+  users: [],
   isFetching: false,
 };
 
@@ -48,6 +52,20 @@ const userReducer = (state = initialState, action) => {
       };
 
     case REQUEST_UPDATE_USER_FAIL:
+      return {
+        ...state,
+        isFetching: false,
+      };
+
+    case REQUEST_USERS_START:
+      return { ...state, isFetching: true };
+    case REQUEST_USERS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        users: action.payload.users,
+      };
+    case REQUEST_USERS_FAIL:
       return {
         ...state,
         isFetching: false,
