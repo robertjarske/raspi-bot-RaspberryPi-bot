1fc1f60935a933d0648e3d4ad9d4f46dd0be836b
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-19 09:16:53 +0200

    add loader container and text

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Loader.css';
 
 const Loader = () => (
-  <div className="spinner">
-    <div className="rect1"></div>
-    <div className="rect2"></div>
-    <div className="rect3"></div>
-    <div className="rect4"></div>
-    <div className="rect5"></div>
-    <h4>Loading...</h4>
+  <div className="loader-container">
+    <div className="loader">
+      <div className="rect1"></div>
+      <div className="rect2"></div>
+      <div className="rect3"></div>
+      <div className="rect4"></div>
+      <div className="rect5"></div>
+      <h4 className="loader-text">Tuning Engine...</h4>
+    </div>
   </div>
 );
 
