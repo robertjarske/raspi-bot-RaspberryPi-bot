16c8a69e707648d2152e33140779a5451028ec90
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-19 10:31:24 +0200

    update notfound view

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Button } from '../../elements';
+import './NotFound.css';
 
 const NotFound = ({ ...props }) => (
-  <div>This is NotFound</div>
+  <div className="notfound">
+    <div className="notfound-icon"></div>
+    <h3 className="notfound-text">Oops, something went south, there are no robots here...</h3>
+    <Button appearance="success" onClick={() => props.history.goBack()}>Go Back</Button>
+  </div>
 );
 
 export default NotFound;
