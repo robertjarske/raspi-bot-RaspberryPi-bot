4b0d863803682d9f6bec73e756dd4572bbf57644
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-21 16:21:42 +0200

    add robot reducer to combined reducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import auth from './auth/authReducer';
 import user from './user/userReducer';
 import notifications from './notifications/notificationsReducer';
+import robots from './robots/robotsReducer';
 
 const nodeBotApp = combineReducers({
   auth,
   user,
   notifications,
+  robots,
 });
 
 export default nodeBotApp;
