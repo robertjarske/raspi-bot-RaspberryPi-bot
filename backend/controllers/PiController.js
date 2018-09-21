4620a1f58507f0be64e17dccb95adc4203b1e94c
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-21 16:18:40 +0200

    add picontroller

---
+++ backend/controllers/PiController.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Robot from '../models/Robot';
+
+
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+/** Robot Login */
+router.post('/', (req, res) => {
+  const { username } = req.body;
+  console.log(username);
+
+  Robot.findOne({ name: username })
+    .then(robot => res.status(200).send({ robot }))
+    .catch(e => res.status(500).send({ msg: e }));
+});
+
+router.post('/logout', (req, res) => {
+  const { id } = req.body;
+
+  console.log(':::::PI LOGOUT::::::');
+  Robot.findOneAndUpdate({ _id: id },
+    {
+      isOnline: false,
+      isAvailable: false,
+    },
+    { new: true })
+    .then(updatedRobot => res.status(200).send({ msgType: 'success', msg: 'Successfully logged out', robot: updatedRobot }))
+    .catch(e => res.status(500).send({ msg: e }));
+});
+
+/** Update db from robot */
+router.put('/', (req, res) => {
+  const { id, newUrl } = req.body;
+
+  Robot.findOneAndUpdate({ _id: id },
+    {
+      url: newUrl,
+      isOnline: true,
+    },
+    { new: true })
+    .then(updatedRobot => res.status(200).send({ msgType: 'success', msg: 'Successfully updated', robot: updatedRobot }))
+    .catch(e => res.status(500).send({ msg: e }));
+});
+
+module.exports = router;
