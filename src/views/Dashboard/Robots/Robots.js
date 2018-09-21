954d79a48d9217f5cc28f76cea1d4142870c3b0a
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-21 16:22:58 +0200

    add robot view and styles

--- src/views/Dashboard/Robots/Robots.js
+++ src/views/Dashboard/Robots/Robots.js
@@ -1,11 +1,115 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { requestRobots, requestAlive, requestMakeUnavailable } from '../../../redux/robots/actions';
+import { Loader, Modal } from '../../../components';
+import { Button } from '../../../elements';
+import './Robots.css';
 
-const Robots = ({ ...props }) => {
-  console.log(props);
-  return (
-    <div style={{
-      width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black',
-    }}>This is Robots</div>
-  );
-};
-export default Robots;
+
+const mapStateToProps = state => ({
+  robots: state.robots,
+  isFetching: state.notifications.isFetching,
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestRobots: () => dispatch(requestRobots()),
+  requestAlive: robot => dispatch(requestAlive(robot)),
+  requestMakeUnavailable: id => dispatch(requestMakeUnavailable(id)),
+});
+
+class Robots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRobot: {},
+      modalOpen: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.props.requestRobots();
+    }, 5000);
+    this.props.requestRobots();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    clearInterval(this.aliveTimer);
+  }
+
+  handleClick(robot) {
+    this.aliveTimer = setInterval(() => {
+      this.props.requestAlive(robot);
+    }, 10000);
+    this.setState({
+      activeRobot: robot,
+    });
+
+    this.toggleModal(null, true);
+  }
+
+
+  toggleModal(e, shouldToggle) {
+    if (shouldToggle) {
+      if (this.state.modalOpen) {
+        clearInterval(this.aliveTimer);
+        this.setState({
+          activeRobot: {},
+        });
+      }
+
+      this.setState({
+        modalOpen: !this.state.modalOpen,
+      });
+    }
+  }
+
+  render() {
+    const { activeRobot } = this.state;
+
+    if (!this.props.robots.robots.length) {
+      return <Loader />;
+    }
+
+    return (
+      <div style={{ width: '100%' }}>
+        {this.state.modalOpen
+          ? <Modal closeModal={this.toggleModal}>
+                <div>
+                    <div className="status-container">
+                      <h5 className="status-text">Status:</h5>
+                      {activeRobot.isOnline
+                        ? <div className="robot-available">
+                          {activeRobot.isAvailable ? '' : <div className="robot-busy"></div>}
+                        </div>
+                        : <div className="robot-unavailable"></div>
+                      }
+                      </div>
+                  <h3 style={{ width: '300px' }}>{activeRobot.name}</h3>
+                  <p style={{ width: '300px' }}>{activeRobot.url}</p>
+                  {activeRobot.isAvailable ? <Button><Link onClick={() => this.props.requestMakeUnavailable(activeRobot._id)} to={`/dashboard/session/${activeRobot._id}`}>Create Session</Link></Button> : <Button><Link to={`/dashboard/watchsession/${activeRobot._id}`}>Watch Session</Link></Button>}
+
+                </div>
+              </Modal> : ''}
+        <div className="robots">
+        {this.props.robots.robots.map((robot, i) => (
+          <div key={i}>
+            <div className="robot-card" onClick={() => this.handleClick(robot)}>
+              <h4>
+                {robot.name.toUpperCase()}
+              </h4>
+              <p>{robot.url}</p>
+          </div>
+          </div>
+        ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Robots);
