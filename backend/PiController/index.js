64ca16ce287867638ecee065a9acc0f0221fdc7e
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-23 13:03:06 +0200

    add localtunnel for pi and fork a child process for cam streaming

--- backend/PiController/index.js
+++ backend/PiController/index.js
@@ -1,31 +1,77 @@
 const express = require('express');
-const raspividStream = require('raspivid-stream');
+const path = require('path');
 const http = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const exec = require('child_process').exec;
+const fetch = require('node-fetch');
+const { fork } = require('child_process');
+
+
+require('dotenv').config();
 
 const app = express();
 const server = http.Server(app);
 const socketIO = require('socket.io-client');
 
-const io = socketIO('http://10.126.4.167:8000/123'); // THIS IS DEV LOCAL AND NAMESPACE 123
-// const wss = require('express-ws')(app, server);
+const io = socketIO(`${process.env.DEV_API_URL}`); // THIS IS DEV LOCAL AND NAMESPACE 123
+
+const port = process.env.PORT || 8000;
+
+const id = '5b8cf3873b05fb0961de4487';
 
 /** Middleware */
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const localtunnel = require('localtunnel');
+
+const tunnel = localtunnel(port, (err, t) => {
+  if (err) console.log(':::PI TUNNEL ERROR :::', err);
+
+  console.log('Pi is up over @: ', t.url);
+  fetch(`${process.env.DEV_API_URL}/pi/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, url: t.url, streamUrl: '' }),
+  })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(e => console.error(e));
+});
+
+const onExit = () => {
+  tunnel.close();
+};
+
+
+tunnel.on('close', () => {
+  console.log('in close tunnel...');
+  fetch(`${process.env.DEV_API_URL}/pi/logout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id }),
+  })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(e => console.error(e));
+});
 
 /** Python shell to run python script from node */
 const { PythonShell } = require('python-shell');
+require('./cleanup').Cleanup(onExit);
 
 const options = {
   mode: 'text',
   pythonPath: '/usr/bin/python',
   pythonOptions: ['-u'], // get print results in real-time
 };
-const pyshell = new PythonShell('./motorDriver.py', options);
+const pyshell = new PythonShell('motorDriver.py', options);
 
 pyshell.on('message', (message) => {
   io.emit('robotMessage', message);
@@ -36,33 +82,37 @@ function sendCommand(cmd) {
 }
 
 io.on('connect', () => {
+  if (id) io.emit('room', id); // Join the socket room
   console.log('connected');
-  io.on('command', (cmd) => {
-    console.log('the command triggered', cmd);
-    sendCommand(cmd);
+});
+
+io.on('command', (cmd) => {
+  console.log('the command triggered', cmd);
+  sendCommand(cmd);
+});
+
+io.on('start-stream', () => {
+  const forked = fork('stream.js');
+
+  forked.on('message', (msg) => {
+    console.log('message', msg);
+    fetch(`${process.env.DEV_API_URL}/pi/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, streamUrl: msg.streamUrl }),
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(e => console.error(e));
   });
+});
 
-  io.on('start-stream', () => {
-    console.log('start stream requested');
+app.get('/alive', (req, res) => res.status(200).send(JSON.stringify({ res: 'Pi says hello!' })));
 
-    const videoStream = raspividStream({
-      width: 960,
-      height: 540,
-      rotation: 180,
-      bitrate: 10000000,
-      sharpness: 0,
-    });
-
-    videoStream.on('data', (data) => {
-      io.emit('data', data);
-    });
-
-    io.on('disconnect', () => {
-      console.log('Client left');
-
-      videoStream.removeAllListeners('data');
-    });
-  });
+app.get('/', (req, res) => {
+  res.status(200).send(JSON.stringify({ res: 'Pi says hello!' }));
 });
 
 app.use((err, req, res, next) => {
@@ -70,7 +120,7 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const port = process.env.PORT || 8000;
+
 server.listen(port, () => console.log(`App listening on port ${port}`));
 
 module.exports = server;
