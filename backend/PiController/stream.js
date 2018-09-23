ebab3a56055e472e1ea81264530582c245eb65a1
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-23 13:04:29 +0200

    add separate stream script for camera

---
+++ backend/PiController/stream.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception in stream: ${err}`);
+  console.log(err.stack);
+});
+
+const express = require('express');
+const raspividStream = require('raspivid-stream');
+const path = require('path');
+
+const app = express();
+const wss = require('express-ws')(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.ws('/video-stream', (ws, req) => {
+  console.log('Client connected to stream');
+
+
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '960',
+    height: '540',
+  }));
+
+  const videoStream = raspividStream({
+    rotation: 180,
+    bitrate: 10000000,
+  });
+
+  videoStream.on('data', (data) => {
+    ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+  });
+
+  ws.on('close', () => {
+    console.log('Client left');
+
+    videoStream.removeAllListeners('data');
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  next(err);
+});
+
+const localtunnel = require('localtunnel');
+
+const tunnel = localtunnel(80, (err, t) => {
+  if (err) console.log(':::PI TUNNEL ERROR :::', err);
+
+  console.log(t.url);
+  console.log(t);
+
+  process.send({ streamUrl: t.url });
+});
+
+app.listen(80, () => console.log('Server started on 80'));
