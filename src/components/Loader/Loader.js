37e0b2de1759e4d5a9b45770e548c99905232d60
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-24 13:43:24 +0200

    refactor loader to be able to change text dynamically

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Loader.css';
 
-const Loader = () => (
+const Loader = ({ text = 'Tuning Engine...', ...props }) => (
   <div className="loader-container">
     <div className="loader">
       <div className="rect1"></div>
@@ -9,7 +9,7 @@ const Loader = () => (
       <div className="rect3"></div>
       <div className="rect4"></div>
       <div className="rect5"></div>
-      <h4 className="loader-text">Tuning Engine...</h4>
+      <h4 className="loader-text">{text}</h4>
     </div>
   </div>
 );
