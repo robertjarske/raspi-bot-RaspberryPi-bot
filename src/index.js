96f6c72f8105d23b025b40d9da16f50f7e4e2c3a
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-24 16:31:27 +0200

    removed dotenv config

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-require('dotenv').config();
+// require('dotenv').config();
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
