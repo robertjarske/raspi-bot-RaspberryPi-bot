b7ae83678883eb6ae08c37455b1e09403ef23f5a
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-24 13:42:53 +0200

    add mobile menu and styling for dashboard

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -7,7 +7,7 @@ const Menu = ({ ...props }) => (
     <div className="bar1"></div>
     <div className="bar2"></div>
     <div className="bar3"></div>
-    {props.activeMenu && !props.isAuthenticated
+    {props.activeMenu && !props.isAuthenticated && !props.dashboardMenu
       ? <nav className="dropdown">
           <ul className="dropdown-ul">
             <li className="dropdown-li"><Link to="/">HOME</Link></li>
@@ -15,7 +15,7 @@ const Menu = ({ ...props }) => (
             <li className="dropdown-li"><Link to="/signup">SIGNUP</Link></li>
           </ul>
         </nav>
-      : props.activeMenu && props.isAuthenticated
+      : props.activeMenu && props.isAuthenticated && !props.dashboardMenu
         ? <nav className="dropdown">
             <ul className="dropdown-ul">
               <li className="dropdown-li"><Link to="/">HOME</Link></li>
@@ -23,7 +23,21 @@ const Menu = ({ ...props }) => (
               <li className="dropdown-li"><p onClick={() => props.logout()}>LOGOUT</p></li>
             </ul>
           </nav>
-        : ''}
+        : props.activeMenu && props.dashboardMenu
+          ? <nav className="dropdown mobile">
+              <ul className="dropdown-ul mobile-ul">
+                <li className="dropdown-li"><Link to="/dashboard">PROFILE</Link></li>
+                <li className="dropdown-li"><Link to="/dashboard/robots">ROBOTS</Link></li>
+                {props.admin
+                  ? <li className="dropdown-li"><Link to="/dashboard/admin">ADMIN</Link></li>
+                  : ''}
+                <li className="dropdown-li"><Link to="/dashboard/developer">DEVELOPER</Link></li>
+                <li className="dropdown-li" onClick={() => props.requestLogout()}>
+                  LOGOUT
+                </li>
+              </ul>
+            </nav>
+          : ''}
   </div>
 );
 
