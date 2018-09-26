1f724f458299e3fe664eca60bc7056a323d39aa9
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-26 10:08:06 +0200

    add folder contained in Pi

---
+++ backend/Pi/README.md
@@ -0,0 +1,6 @@
+# u14-fullstackjs-idea
+
+Fullstack school project, the idea is to build a nodebot using a Raspberry Pi, hooking it up with a camera for live video.
+
+## PI
+This folder is on the Raspberry Pi
---
+++ backend/Pi/stream.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception in stream: ${err}`);
+  console.log(err.stack);
+});
+
+const express = require('express');
+const raspividStream = require('raspivid-stream');
+const path = require('path');
+
+const app = express();
+const wss = require('express-ws')(app);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.ws('/video-stream', (ws, req) => {
+  console.log('Client connected to stream');
+
+
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '960',
+    height: '540',
+  }));
+
+  const videoStream = raspividStream({
+    rotation: 180,
+    bitrate: 10000000,
+  });
+
+  videoStream.on('data', (data) => {
+    ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+  });
+
+  ws.on('close', () => {
+    console.log('Client left');
+
+    videoStream.removeAllListeners('data');
+  });
+});
+
+process.on('message', (msg) => {
+  if (msg.stop === 'stop') {
+    process.exit()
+  };
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  next(err);
+});
+
+const ngrok = require('ngrok');
+(async function() {
+  const url = await ngrok.connect();
+  const streamUrl = `${url.slice(6)}/video-stream`;
+
+  process.send({ streamUrl });
+})();
+
+app.listen(80, () => console.log('Server started on 80'));
