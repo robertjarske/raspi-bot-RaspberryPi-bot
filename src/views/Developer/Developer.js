9e347465ca671ac5d4b6c4f9530ebb2861d05dfa
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-09-27 16:56:30 +0200

    remove last mediaquery for modal since it broke layout, texxt align center on dev view

--- src/views/Developer/Developer.js
+++ src/views/Developer/Developer.js
@@ -5,7 +5,7 @@ const Developer = () => (
   <div className="developer">
     <div className="developerAvatar"></div>
     <p>This project is made as a school assignment during my time at Chas Academy.</p>
-    <p>Project repo: <a className="repo-link" href="https://github.com/chas-academy/14-fullstackjs-idea-robertjarske">14-fullstackjs-idea-robertjarske</a></p>
+    <p>Project repo: <a className="repo-link" rel="noopener noreferrer" target="_blank" href="https://github.com/chas-academy/14-fullstackjs-idea-robertjarske">14-fullstackjs-idea-robertjarske</a></p>
   </div>
 );
 
